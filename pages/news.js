48ef89ee43d9e78c6e833acf137d8cbbc4d4c23c
Luigi Matias <igiboi26@gmail.com>
2022-11-16 15:41:48 -0500

    learning next.js

---
+++ pages/news.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const NewsPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>News</title>
+      </Head>
+      <h1>News Page</h1>
+    </div>
+  );
+};
+
+export default NewsPage;
